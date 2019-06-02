4e8cfd64b61f32522c75a2d961a50546facd36f7
rupeshsahu361 <rsahu3525@gmail.com>
2019-06-02 11:41:52 +0530

    my very first commit

---
+++ fi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home (Hotel Management)</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="img/LOGO.png" type="img/icon" rel="icon">
+</head>
+<body>
+	<div id="full">
+		<div id="bg" style="background-image: url('img/bed.jpg'); height: 1200px;">
+		<div id="header">
+			<div id="logo">
+				<h1><font color="white">My Project</font></h1>
+			</div>
+			<div id="nav">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="contact.php">Contect Us</a></li>
+					<li><a href="r1.php">BOOK</a></li>
+					<li><a href="#">Our Hotels</a></li>
+					<li><a href="#">Help</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="banner">
+			<div id="form"><br><br><br><br><br>
+				<center><h1 style="text-align: center;color: blue;">Registration Completed</h1></center>
+					</div>
+		</div>
+	</div>
+	</div>
+	
+  </div>
+</body>
+
+</html>
